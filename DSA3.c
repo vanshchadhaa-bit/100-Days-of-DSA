d640b16cc83fea9e33870c08a0eada2e30301158
Vansh Chadha <vanshchadhaa@gmail.com>
2026-02-03 21:33:41 +0530

    Create DSA3.c

---
+++ DSA3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int k;
+    scanf("%d", &k);
+    int comparisons = 0;
+    int found = -1;
+    for(int i = 0; i < n; i++) {
+        comparisons++;
+        if(arr[i] == k) {
+            found = i;
+            break;
+        }
+    }
+    if(found != -1) {
+        printf("Found at index %d\n", found);
+    } else {
+        printf("Not Found\n");
+    }
+    printf("Comparisons = %d\n", comparisons);
+    return 0;
+}
