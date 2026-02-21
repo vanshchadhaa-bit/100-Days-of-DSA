871257b81d9fcea1dc014187499d5e409ee65fd5
Vansh Chadha <vanshchadhaa@gmail.com>
2026-02-22 00:52:57 +0530

    Create DSA17.c

---
+++ DSA17.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int arr[100];  // Assuming n <= 100 based on examples
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    int max_val = arr[0];
+    int min_val = arr[0];
+    
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max_val) {
+            max_val = arr[i];
+        }
+        if(arr[i] < min_val) {
+            min_val = arr[i];
+        }
+    }
+    
+    printf("Max: %d\n", max_val);
+    printf("Min: %d\n", min_val);
+    
+    return 0;
+}
