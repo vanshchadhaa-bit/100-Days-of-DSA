662ba7b549dd21d5dd973a6270c899cc81f2ca76
Vansh Chadha <vanshchadhaa@gmail.com>
2026-03-23 00:05:58 +0530

    Create DSA32.c

---
+++ DSA32.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#define MAX 1000
+
+int stack[MAX];
+int top = -1;
+
+void push(int val) {
+    stack[++top] = val;
+}
+
+void pop() {
+    if (top != -1)
+        top--;
+}
+
+void display() {
+    for (int i = top; i >= 0; i--)
+        printf("%d ", stack[i]);
+}
+
+int main() {
+    int n, m, x;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        push(x);
+    }
+
+    scanf("%d", &m);
+
+    for (int i = 0; i < m; i++) {
+        pop();
+    }
+
+    display();
+
+    return 0;
+}
